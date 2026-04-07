1b271cc1ab11f8a8abdebeef86c84548268fd22f
kadoozin <kadoozin@gmail.com>
2026-04-07 15:44:38 -0300

    feat: implementar entidades Paciente, Contato e Agendamento com relacionamentos

---
+++ src/main/java/com/example/agendamento_consultas/database/model/Agendamento.java
@@ -0,0 +1,32 @@
+package com.example.agendamento_consultas.database.model;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.time.LocalDate;
+import java.time.LocalTime;
+
+@Entity
+@AllArgsConstructor
+@NoArgsConstructor
+@Getter
+@Setter
+@Table(name = "tb_agendamento")
+public class Agendamento {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @ManyToOne
+    @JoinColumn(nullable = false)
+    private Paciente paciente;
+
+    @Column(nullable = false)
+    private LocalDate data;
+
+    @Column(nullable = false)
+    private LocalTime horario;
+}
---
+++ src/main/java/com/example/agendamento_consultas/database/model/Contato.java
@@ -0,0 +1,25 @@
+package com.example.agendamento_consultas.database.model;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Entity
+@AllArgsConstructor
+@NoArgsConstructor
+@Getter
+@Setter
+@Table(name = "tb_contato")
+public class Contato {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(unique = true, nullable = false)
+    private String email;
+
+    @Column(nullable = false)
+    private String numero;
+}
