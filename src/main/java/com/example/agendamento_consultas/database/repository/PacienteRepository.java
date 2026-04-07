fbcc658cfe54f8fec62ca4b015387885e6c37c67
kadoozin <kadoozin@gmail.com>
2026-04-07 18:35:24 -0300

    feat: adicionar repositories de Paciente, Contato e Agendamento

---
+++ src/main/java/com/example/agendamento_consultas/database/repository/PacienteRepository.java
@@ -0,0 +1,9 @@
+package com.example.agendamento_consultas.database.repository;
+
+import com.example.agendamento_consultas.database.model.Paciente;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface PacienteRepository extends JpaRepository<Long, Paciente> {
+}
