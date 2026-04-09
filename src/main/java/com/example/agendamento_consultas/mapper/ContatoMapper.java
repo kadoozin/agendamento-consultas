e82f0e038d58d088cb547009f2bc79cb69f19a34
kadoozin <kadoozin@gmail.com>
2026-04-09 11:52:44 -0300

    feat: adicionar mappers das entidades Paciente, Contato e Agendamento

---
+++ src/main/java/com/example/agendamento_consultas/mapper/ContatoMapper.java
@@ -0,0 +1,23 @@
+package com.example.agendamento_consultas.mapper;
+
+import com.example.agendamento_consultas.database.model.Contato;
+import com.example.agendamento_consultas.dto.request.ContatoRequest;
+import com.example.agendamento_consultas.dto.response.ContatoResponse;
+import org.mapstruct.BeanMapping;
+import org.mapstruct.Mapper;
+import org.mapstruct.MappingTarget;
+import org.mapstruct.NullValuePropertyMappingStrategy;
+
+import java.util.List;
+
+@Mapper(componentModel = "spring")
+public interface ContatoMapper {
+    ContatoResponse toResponse(Contato contato);
+
+    Contato toEntity(ContatoRequest request);
+
+    @BeanMapping(nullValuePropertyMappingStrategy = NullValuePropertyMappingStrategy.IGNORE)
+    void updateEntity(ContatoRequest request, @MappingTarget Contato contato);
+
+    List<ContatoResponse> toResponseList(List<Contato> contatos);
+}
